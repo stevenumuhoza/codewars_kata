739de539b59bcd13cb4ef01f0a34c9838319f73b
stevenumuhoza <stevenumuhoza@gmail.com>
2026-09-04 06:55:23 +0200

    Add solution - CodeHub

---
+++ 8-kyu/counting-sheep-dot-dot-dot/counting-sheep-dot-dot-dot.js
@@ -0,0 +1,11 @@
+function countSheeps(sheep) {
+    let count = 0;
+    
+    for (let i = 0; i < sheep.length; i++) {
+        if (sheep[i] === true) {
+            count++;
+        }
+    }
+    
+    return count;
+}
